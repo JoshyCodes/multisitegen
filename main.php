77275708fe945090d4276b994279b9a590826ee2
root <jfischeronline@gmail.com>
2020-06-17 15:54:20 +0000

    init repo

---
+++ main.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: Site Builder
+Plugin URI:
+Description: multisite site creator of SAI pilot
+Version: 0.0.1
+Author: J.Fischer
+Author URI: rainyday.media
+============================================================================================================
+This software is provided "as is" and any express or implied warranties, including, but not limited to, the
+implied warranties of merchantibility and fitness for a particular purpose are disclaimed. In no event shall
+the copyright owner or contributors be liable for any direct, indirect, incidental, special, exemplary, or
+consequential damages(including, but not limited to, procurement of substitute goods or services; loss of
+use, data, or profits; or business interruption) however caused and on any theory of liability, whether in
+contract, strict liability, or tort(including negligence or otherwise) arising in any way out of the use of
+this software, even if advised of the possibility of such damage.
+============================================================================================================
+*/
+/**
+ * Type	: Plugin
+ * Name	: Site Builder
+ *
+ * @since 0.0.1
+ * @author Rainy Day Media
+ */
+/**
+ * Create namespace and include supporting files and functions
+ */
+
+//include plugin_dir_path( __FILE__ ) . '/resources/sefsForm.php';
+include plugin_dir_path( __FILE__ ) . '/resources/sBuilder.php';
---
+++ resources/sBuilder.php
@@ -0,0 +1,12 @@
+<?php
+
+function createMultiSite () {
+    // run wp create site with slug name...
+}
+
+function activateRestfulKey () {
+    // add to db with user/pass for Api key...
+}
+
+global $wpdb;
+echo $wpdb->prefix;
